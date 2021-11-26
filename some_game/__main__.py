3c11a925db07a7f36f11830771cfb0bd0a3fd444
Row84M <stewy.rowley@gmail.com>
2021-11-26 15:52:16 -0700

    update

---
+++ some_game/__main__.py
@@ -0,0 +1,70 @@
+import os
+
+
+os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\StewM\Documents\BYU Idaho\Fall 2021\programmingCourses\CSE210_personal\cseFinalProject\some_game\raylib-2.0.0-Win64-mingw\lib'
+
+import random
+from game import constants
+from game.director import Director
+from game.actor import Actor
+from game.point import Point
+from game.draw_actors_action import DrawActorsAction
+from game.input_service import InputService
+from game.output_service import OutputService
+from game.physics_service import PhysicsService
+from game.audio_service import AudioService
+
+# TODO: Add imports similar to the following when you create these classes
+from game.scoreboard import ScoreBoard
+# from game.brick import Brick
+# from game.ball import Ball
+# from game.paddle import Paddle
+# from game.control_actors_action import ControlActorsAction
+# from game.handle_collisions_action import HandleCollisionsAction
+# from game.handle_off_screen_action import HandleOffScreenAction
+# from game.move_actors_action import MoveActorsAction
+
+def main():
+
+    # create the cast {key: tag, value: list}
+    cast = {}
+
+    cast["bricks"] = []
+    # TODO: Create bricks here and add them to the list
+
+    cast["balls"] = []
+    # TODO: Create a ball here and add it to the list
+
+    cast["paddle"] = []
+    # TODO: Create a paddle here and add it to the list
+
+    # Create the script {key: tag, value: list}
+    script = {}
+
+    input_service = InputService()
+    output_service = OutputService()
+    physics_service = PhysicsService()
+    audio_service = AudioService()
+
+    draw_actors_action = DrawActorsAction(output_service)
+
+    # TODO: Create additional actions here and add them to the script
+
+    script["input"] = []
+    script["update"] = []
+    script["output"] = [draw_actors_action]
+
+
+
+    # Start the game
+    output_service.open_window("Batter");
+    audio_service.start_audio()
+    audio_service.play_sound(constants.SOUND_START)
+    
+    director = Director(cast, script)
+    director.start_game()
+
+    audio_service.stop_audio()
+
+if __name__ == "__main__":
+    main()
